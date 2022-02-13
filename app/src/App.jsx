35b0207b0ada4a25a45094400414358254064899
Moises Silva <moises.silva@cargobrasil.log.br>
2022-02-13 10:43:29 -0300

    componente card criado

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,12 @@
 import './App.css';
+import Card from './components/card/Card'
 
 function App() {
   return (
     <div className="App">
-git
+      <Card />
+      <Card />
+      <Card />
     </div>
   );
 }
