8f18ce1174fc7cdab648a33225a139c6a1145e6b
Moises Silva <moises.silva3000@outlook.com>
2022-02-14 17:46:42 -0300

    lib material ui adicionada + componente form login

--- app/src/views/login/Form.jsx
+++ app/src/views/login/Form.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Input from '@mui/material/Input';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import LockIcon from '@mui/icons-material/Lock';
+import Button from '@mui/material/Button';
 
-const FormLogin = () => {
 
+
+export default function FormLogin() {
     return (
-        <div>
+        <>
+            <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                <FormControl variant="standard">
+                    <InputLabel htmlFor="input-with-icon-adornment">
+                        e-mail
+                    </InputLabel>
+                    <Input
+                        type="email" id="input-with-icon-adornment"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <AccountCircle />
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
+                <FormControl variant="standard">
+                    <InputLabel htmlFor="input-with-icon-adornment">
+                        senha
+                    </InputLabel>
+                    <Input
+                        type="password" id="input-with-icon-adornment"
+                        startAdornment={
+                            <InputAdornment position="start">
+                                <LockIcon />
+                            </InputAdornment>
+                        }
+                    />
+                </FormControl>
 
-        </div>
+            <Button variant="contained"> Olá Mundo</Button>
+            </Box>
+        </>
     );
 }
-export default FormLogin;
