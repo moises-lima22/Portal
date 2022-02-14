c6077c92e2c2e91ab46ffc73bc507c58722a90b0
Moises Silva <moises.silva@cargobrasil.log.br>
2022-02-13 21:19:47 -0300

    iniciando pager login

---
+++ app/src/components/buttom/Buttom.jsx
@@ -0,0 +1,11 @@
+const Buttom = ({ text }) => {
+    return (
+        <>
+            <div className="btn">
+                <button>{text}</button>
+            </div>
+        </>
+    );
+}
+
+export default Buttom;
--- app/src/views/login/Login.jsx
+++ app/src/views/login/Login.jsx
@@ -1,12 +1,28 @@
 import React from "react";
+import TextField from '@material-ui/core/TextField';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+
 import SvgComponent from "./animation"
 
+import "./Login.css"
 
 const Login = () => {
     return (
-        <>
-            <SvgComponent />
-        </>
+        <div className="container-login">
+            <div className="form-login">
+                <img src={require("../../assets/logo.png")} alt="" srcset="" />
+                <AccountCircle />
+                <TextField id="input-with-icon-grid" label="With a grid" />
+                <TextField id="standard-basic" label="Standard" />
+
+            </div>
+            <div className="img-login">
+                <SvgComponent className="img"/>
+            </div>
+
+
+        </div>
     );
 }
 
